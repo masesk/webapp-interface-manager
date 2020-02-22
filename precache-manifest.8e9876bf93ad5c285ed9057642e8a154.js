99bd39f44d1c0ce661610279459b227908777909
Mases Krikorian <maseskrikorian@gmail.com>
2020-02-22 11:53:50 -0800

    v0.2

---
+++ precache-manifest.8e9876bf93ad5c285ed9057642e8a154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049f5ddc16a39f93b2934f364048056b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "932eb112d38d870b91c6",
+    "url": "./static/css/main.9a25ccaa.chunk.css"
+  },
+  {
+    "revision": "9d8fadc08fb44f87b03a",
+    "url": "./static/js/2.d65fe40d.chunk.js"
+  },
+  {
+    "revision": "47a00163856d2c9df4d563d6a5a069d2",
+    "url": "./static/js/2.d65fe40d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932eb112d38d870b91c6",
+    "url": "./static/js/main.5adc2d85.chunk.js"
+  },
+  {
+    "revision": "2dbf94dcb30861048927",
+    "url": "./static/js/runtime-main.757f061e.js"
+  }
+]);
