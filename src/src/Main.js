c082cf70b10ebeb86020bfd6053d6feddb753ff5
Mases Krikorian <maseskrikorian@gmail.com>
2020-02-23 00:42:47 -0800

    initial

---
+++ src/src/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/App.css';
+import Window from './Window'
+
+
+function App() {
+  return (
+    <>
+      <Window />
+      <Window />
+
+    </>
+  );
+}
+
+export default App;
+
+
